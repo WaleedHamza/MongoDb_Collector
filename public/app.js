4c7f9d0da81c17fe51e10a83b067e9679d8b7fd0
Waleed Hamza <hamzah.waleed@gmail.com>
2018-07-06 22:35:03 -0400

    +homeModal

--- public/app.js
+++ public/app.js
@@ -1,21 +1,12 @@
 //Make AJAX call to get all scraped data from the BSON 
 
 $(()=>{
+//Bootstrap Modal Function
+$('#myModal').on('shown.bs.modal', ()=> {
+    $('#myInput').trigger('focus')
+  });
 
-    // $.getJSON("/all", (data)=> {
-    //     console.log("this is data",data);
-    //     for (let i = 0 ; i < data.length; i++){
-    //         link.push(dbArticle.data[i].link)
-    //         $("#results").append(
-    //             `
-    //            <tr><td>${data[i].title}</td></tr>
-    //            <tr><td>${data[i].link}</td></tr>
-    //            <tr><td><button class="saveBtn" data-title="${data[i].title}"  data-link="${data[i].link}">Save</button></td></tr>
-    //             `
-    //         )
-    //     }
-    // });
-   
+
 
 $(document).on('click', ".saveBtn", function(){
     $('#notes').empty();
